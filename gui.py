def1199621e3770d242542a38d241a1af9c1f3a0
Charlie <charlieedoherty@gmail.com>
2023-11-29 13:27:55 -0600

    Make a change without including \mega other child directories

--- gui.py
+++ gui.py
@@ -7,3 +7,4 @@ input_box = SGui.Input(tooltip="Enter a Todo here")
 window = SGui.Window("GUI Todo App", layout=[[label], [input_box]])
 window.read()
 window.close()
+# Make a change without including \mega other child directories
