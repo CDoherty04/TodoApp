e8631ba64fd55696fad8a4ecf5752057b941daff
Charlie Doherty <charlieedoherty@gmail.com>
2024-01-22 00:23:07 -0600

    Implement the "Add" feature for the web app and ensure every line is stripped before writing to file in functions

--- webapp.py
+++ webapp.py
@@ -1,18 +1,27 @@
 import streamlit as st
-import random
+from random import choice
+import functions
+
+todos = functions.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["new_todo"]
+    todos.append(todo)
+    functions.write_to_file(todos)
+
 
 st.title("Charlie's Todo App")
 st.subheader("For all of my organizational needs")
 
-with open("data.txt", "r") as file:
-    todos = file.readlines()
-
-for todo in todos:
-    st.checkbox(todo)
+for task in todos:
+    st.checkbox(task)
 
 placeholders = ["Finish creating my web app", "Take the trash out",
                 "Learn how to cook Dad's spaghetti", "WOOOOOOOOOOOOO",
                 "Organize game night", "Volunteer for my church"]
 
-placeholder = random.choice(placeholders)
-st.text_input(label="Enter a Todo", placeholder=placeholder)
+placeholder = choice(placeholders)
+st.text_input(label="Enter a Todo", placeholder=placeholder, on_change=add_todo, key="new_todo")
+
+
